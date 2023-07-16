0b4af8842db967afd375ccd44cf56a8270f09227
Matt Arm <matthewarmstrong1998@gmail.com>
2023-07-16 02:29:00 -0400

    working

--- src/Driver.java
+++ src/Driver.java
@@ -4,9 +4,17 @@ public class Driver {
 	public static void main(String[] args) {
 		int[] req = {5,6,17,20};
 		int[] io = {1,1,1,2};
-		PCB test = new PCB(1, 100, req, io);
+		PCB test1 = new PCB(1, 100, req, io);
+		int[] req2 = {};
+		int[] io2 = {};
+		PCB test2 = new PCB(2, 67, req2, io2);
+		int[] req3 = {14,20,21,22,23,140,145};
+		int[] io3 = {1,2,2,2,2,2,1};
+		PCB test3 = new PCB(3, 150, req3, io3);
 		Scheduler s = new Scheduler();
-		s.admit(test);
+		s.admit(test1);
+		s.admit(test2);
+		s.admit(test3);
 		s.runScheduler();
 	}
 
